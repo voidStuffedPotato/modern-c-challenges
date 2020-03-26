6bf44241db9900fb1c7fba430ea0b23be785f856
Volokhov Artyom <volohov.art@gmail.com>
2020-03-27 01:10:36 +0300

    Implemented Gaussian elimination

--- 6_Linear-algebra/matrices.c
+++ 6_Linear-algebra/matrices.c
@@ -1,32 +1,74 @@
 /*
- *  Some of the most important problems for which arrays are uset stem from
+ * Some of the most important problems for which arrays are used stem from
  * linear algebra. Can you write functions that do vector-to-vector or
  * matrix-to-vector products at this point? What about Gauss elimination or
  * iterative algorithms for matrix inversion?
- * TODO: matrix inversion and Gauss elimination, tests
+ * TODO: matrix inversion
  */
 
 #include <malloc.h>
-#include <stddef.h>
+#include <stdbool.h>
 #include <stdio.h>
-#include <stdlib.h>
 
-double scalprod(size_t n, double a[n], double b[n]);
-double *vectomat(size_t m, size_t n, double vec[m], double arr[m][n]);
-void vectorwsum(size_t n, double a[n], double b[n], double lambda);
-void vectoscal(size_t n, double a[n], double lambda);
 double **eye(size_t n);
+void swap(size_t n, double a[n], double b[n]);
+double scal_prod(size_t n, double a[n], double b[n]);
+double *vec_to_mat(size_t m, size_t n, double vec[m], double arr[m][n]);
+void vec_to_scal(size_t n, double a[n], double lambda);
+void vec_w_sum(size_t n, double a[n], double b[n], double lambda);
+int gauss_elimination(size_t m, size_t n, double arr[m][n]);
+int inverse(size_t n, double[n][n]);
+double **mat_to_mat(size_t m, size_t n, size_t k, double A[m][n],
+                    double B[n][k]);
+void print_vec(size_t n, double a[n]);
+void print_arr(size_t m, size_t n, double arr[m][n]);
+void print_mat(size_t m, size_t n, double** mat);
 
-int main() {}
+int main() {
+    double A[4][4] = {[0] = {1, 0, 1, 0},
+                      [1] = {0, 0, 1, 0},
+                      [2] = {0, 2, 1, 0},
+                      [3] = {1, 4.5, 3, 1}
+                     };
+    int ret = gauss_elimination(4, 4, A);
+    double b[4][4] = {{1,2,3,4},{2,3,4,5},{3,4,5,6},{4,5,6,7}};
+    if (ret == 0) {
+        print_arr(4, 4, A);
+    }
+    double** c = mat_to_mat(4, 4, 4, b, b);
+    print_mat(4, 4, c);
+    free(c);
+    return 0;
+}
 
-double scalprod(size_t n, double a[n], double b[n]) {
+void print_vec(size_t n, double a[n]) {
+    for (size_t i = 0; i < n; ++i)
+        printf("%.3f\t", a[i]);
+    printf("\n");
+}
+
+void print_arr(size_t m, size_t n, double arr[m][n]) {
+    printf("{\n");
+    for (size_t i = 0; i < m; i++)
+        print_vec(n, arr[i]);
+    printf("}\n");
+}
+
+void print_mat(size_t m, size_t n, double **arr) {
+    printf("{\n");
+    for (size_t i = 0; i < m; i++)
+        print_vec(n, arr[i]);
+    printf("}\n");
+}
+
+double scal_prod(size_t n, double a[n], double b[n]) {
     double res = 0;
     for (size_t i = 0; i < n; ++i)
         res += a[i] * b[i];
     return res;
 }
 
-double *vectomat(size_t m, size_t n, double vec[m], double arr[m][n]) {
+double *vec_to_mat(size_t m, size_t n, double vec[m], double arr[m][n]) {
     double *ret_vec = calloc(n, sizeof(double));
     if (ret_vec) {
         for (size_t i = 0; i < n; ++i) {
@@ -40,12 +82,12 @@ double *vectomat(size_t m, size_t n, double vec[m], double arr[m][n]) {
     return ret_vec;
 }
 
-void vectorwsum(size_t n, double a[n], double b[n], double lambda) {
+void vec_w_sum(size_t n, double a[n], double b[n], double lambda) {
     for (size_t i = 0; i < n; ++i)
         a[i] += lambda * b[i];
 }
 
-void vectoscal(size_t n, double a[n], double lambda) {
+void vec_to_scal(size_t n, double a[n], double lambda) {
     for (size_t i = 0; i < n; ++i)
         a[i] *= lambda;
 }
@@ -63,4 +105,44 @@ double **eye(size_t n) {
             }
     }
     return ret_mat;
+}
+
+void swap(size_t n, double a[n], double b[n]) {
+    for (size_t i = 0; i < n; ++i) {
+        double tmp = a[i];
+        a[i] = b[i];
+        b[i] = tmp;
+    }
+}
+
+int gauss_elimination(size_t m, size_t n, double arr[m][n]) {
+    for (size_t j = 0; j < n; ++j) {
+        bool found = false;
+        size_t nonzero = 0;
+        for (size_t i = j; i < m; ++i) {
+            if (arr[i][j] != 0) {
+                found = true;
+                nonzero = i;
+                break;
+            }
+        }
+        if (found) {
+            swap(n, arr[j], arr[nonzero]);
+            vec_to_scal(n, arr[j], arr[j][j] ? 1.0 / arr[j][j] : 1.0);
+            for (size_t i = j + 1; i < n; ++i) {
+                vec_w_sum(n, arr[i], arr[j], -1.0 * arr[i][j]);
+            }
+        } else
+            return -1;
+    }
+    return 0;
+}
+
+double **mat_to_mat(size_t m, size_t n, size_t k, double A[m][n],
+                    double B[n][k]) {
+    double **ret_val = 0;
+    ret_val = calloc(m, sizeof(double*));
+    for (size_t i = 0; i < m; ++i)
+        ret_val[i] = vec_to_mat(n, k, A[i], B);
+    return ret_val;
 }
